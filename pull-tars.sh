4b6d6f7ac3c168f255bbebdcb8347827fefc0b21
AJ Bagwell <anthony.bagwell@hivehome.com>
2019-12-03 12:28:50 +0000

    Create scripts for pulling version from tars

---
+++ pull-tars.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s extglob
+
+cd $(realpath $(dirname $0))
+
+dir_url="https://opensource.apple.com/tarballs/mDNSResponder/"
+
+for file in $(curl -SsL $dir_url | sed -nE 's|.*href="(mDNS[^"]+)".*|\1|p'); do
+  version="$(echo $file | sed -E 's|mDNSResponder-(.*).tar.gz|\1|')"
+  dir="mDNSResponder-$version"
+
+  if ! git tag -l | grep -q "$version"; then
+    rm -rf ./!(pull-tars.sh)
+
+    file_url="$dir_url$file"
+    wget $file_url
+    tar -xf $file
+    rm $file
+    mv $dir/* .
+    rmdir $dir
+    git add .
+    git commit -m "update to version $version from $file"
+    git tag "$version"
+  fi
+done
+
+
+
